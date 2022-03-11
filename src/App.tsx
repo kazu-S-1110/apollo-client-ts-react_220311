0457b4b03b2746830277faed0e2fdd8fcf7094e3
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2022-03-11 12:51:23 +0900

    ApolloClientの初期化

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
+import React, { useEffect } from 'react';
 import './App.css';
 
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache(),
+});
+const GET_RATES = gql`
+  query GetRates {
+    rates(currency: "USD") {
+      currency
+    }
+  }
+`;
+
 function App() {
+  useEffect(() => {
+    client.query({ query: GET_RATES }).then((result) => console.log(result));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Apollo Client</h1>
     </div>
   );
 }
