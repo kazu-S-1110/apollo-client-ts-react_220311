781336e0d43891cb1f0a37ffce9d4d12b807f9b0
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2022-03-12 23:53:24 +0900

    なんとか自力でuseQueryに成功。まだGraph QLのDocsの読み方に慣れない。

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import {
-  ApolloClient,
-  ApolloProvider,
-  gql,
-  InMemoryCache,
-} from '@apollo/client';
-import React, { useEffect } from 'react';
 import './App.css';
-import { ExchangeRates } from './ExchangeRates';
-
-const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
-  cache: new InMemoryCache(),
-});
-
-const EXCHANGE_RATES = gql`
-  query GetExchangeRates {
-    rates(currency: "USD") {
-      currency
-      rate
-    }
-  }
-`;
+import { GetRepositories } from './components/GetRepositories';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <div>
-        <h1>Apollo Client</h1>
-        <ExchangeRates exchangeRates={EXCHANGE_RATES} />
-      </div>
-    </ApolloProvider>
+    <>
+      <GetRepositories />
+    </>
   );
 }
 
---
+++ src/components/GetRepositories.tsx
@@ -0,0 +1,34 @@
+import { gql, useQuery } from '@apollo/client';
+import React, { VFC } from 'react';
+
+const GET_REPOSITORIES = gql`
+  query {
+    user(login: "kazu-S-1110") {
+      name
+      url
+      repositories(last: 20) {
+        totalCount
+        nodes {
+          name
+          description
+          createdAt
+          updatedAt
+          url
+        }
+      }
+    }
+  }
+`;
+
+export const GetRepositories: VFC = (): JSX.Element => {
+  const { loading, error, data } = useQuery(GET_REPOSITORIES);
+
+  console.log(data);
+
+  return (
+    <>
+      {loading && <p>'Loading...'</p>}
+      {error && `Error! ${error.message}`}
+    </>
+  );
+};
